d124239eda2153e54eae1e961ec9641ef080a220
haijunz <haijunz@sina.com>
2017-10-30 17:02:34 +0800

    python_signal_process

---
+++ ExtraSensory/unbalance_weight.py
@@ -0,0 +1,21 @@
+import numpy as np
+import math
+
+# labels_dict : {ind_label: count_label}
+# mu : parameter to tune 
+
+def create_class_weight(labels_dict,mu=0.15):
+    total = np.sum(labels_dict.values())
+    keys = labels_dict.keys()
+    class_weight = dict()
+
+    for key in keys:
+        score = math.log(mu*total/float(labels_dict[key]))
+        class_weight[key] = score if score > 1.0 else 1.0
+
+    return class_weight
+
+# random labels_dict
+labels_dict = {0: 2813, 1: 78, 2: 2814, 3: 78, 4: 7914, 5: 248, 6: 7914, 7: 248}
+
+print (create_class_weight(labels_dict))
